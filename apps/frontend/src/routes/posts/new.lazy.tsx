6b12c8950ea0e863f78ed1af0ef17fdaf6f504d7
quangbm00 <quangbm@missingcorner.com>
2024-05-05 15:07:22 +0700

    feat(frontend): add page post-new

--- apps/frontend/src/routes/posts/new.lazy.tsx
+++ apps/frontend/src/routes/posts/new.lazy.tsx
@@ -1,5 +1,62 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Header } from '../../components'
 
 export const Route = createLazyFileRoute('/posts/new')({
-  component: () => <div>Hello /posts/new!</div>
+  component: () => <Page />,
 })
+
+function Page() {
+  return (
+    <>
+      <Header />
+
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create a new post
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Content
+                  </label>
+                  <textarea
+                    name="text"
+                    id="text"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write some of your thoughts"
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    htmlFor="file"
+                  >
+                    Upload file
+                  </label>
+                  <input
+                    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    id="file"
+                    type="file"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Upload
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
